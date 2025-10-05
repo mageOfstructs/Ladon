943770f6f16f9250c3fc0f044f09c8be4be2151f
mageOfStructs <jason.puschnig@gmx.at>
2025-10-05 10:03:04 +0200

    add a20 check

--- boot.asm
+++ boot.asm
@@ -3,6 +3,27 @@
 
 mov bx, test
 call prt
+cli
+; disable nmi (bc intel stoopid)
+mov dx, 0x70
+in ax, dx
+or ax, 0x80
+out dx, ax
+
+call ck_a20
+cmp ax, 1
+je a20_done
+
+mov ax, 0x2401 ; an attempt at enabling a20
+int 0x15       ; wiki says to straight up ignore anything this returns and just check manually again
+call ck_a20
+cmp ax, 1
+je a20_done
+
+print a20_fail
+jmp loop
+
+a20_done:
 loop:
   jmp loop
 
--- utils.asm
+++ utils.asm
@@ -17,3 +17,48 @@ prt_end:
   pop ax
   ret
 
+; subroutine to check if A20 line is enabled
+; returns: 1 in ax if it is, otherwise 0
+ck_a20:
+  push ds
+  push es
+  push bx
+  push cx
+
+  mov cx, 0
+
+  xor ax, ax
+  mov ds, ax
+  not ax
+  mov es, ax
+
+  mov bx, 0x7dfe
+  mov ax, [ds:bx]
+  push ax
+  mov byte [ds:bx], 0x00
+
+  add bx, 0x10
+  mov ax, [es:bx]
+  push ax
+  mov byte [es:bx], 0xFF
+
+  cmp byte [ds:bx], 0xFF
+  je ck_a20_off
+
+  mov cx, 1
+
+ck_a20_off:
+  pop ax
+  mov [es:bx], ax
+  sub bx, 0x10
+  pop ax
+  mov [ds:bx], ax
+
+  mov ax, cx
+
+  pop cx
+  pop bx
+  pop es
+  pop ds
+  ret
+
