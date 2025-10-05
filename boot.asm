fb08fd4565333b66b261de38267ba598fdf1645b
mageOfStructs <jason.puschnig@gmx.at>
2025-10-05 10:03:38 +0200

    start work on stage2

--- boot.asm
+++ boot.asm
@@ -1,8 +1,27 @@
+%macro print 1
+mov bx, %1
+call prt
+%endmacro
+
 [bits 16]
 [org 0x7c00]
 
-mov bx, test
-call prt
+push dx ; dl = boot drive
+
+mov ax, 0x7000
+mov ss, ax ; set stack to the highest (usable) address
+mov sp, 0xFFFF
+mov bp, sp
+
+xor ax, ax ; ax -> 0
+mov cs, ax
+mov ds, ax
+mov es, ax
+mov fs, ax
+mov gs, ax
+
+print test
+
 cli
 ; disable nmi (bc intel stoopid)
 mov dx, 0x70
@@ -24,11 +43,72 @@ print a20_fail
 jmp loop
 
 a20_done:
+
+load_stage2:
+pop dx
+mov ah, 8 ; get drive geometry
+int 0x13
+
+mov ah, 2
+
+int 0x13
+
+lgdt [GDT_desc]
+
+print gdt
+
+; enter protected mode
+mov eax, cr0
+or al, 1
+mov cr0, eax
+jmp (GDT_code-GDT_start):prot_main
+
 loop:
   jmp loop
 
+GDT_start:
+  times 2 dd 0  ; null entry
+GDT_code:
+  dw 0xFFFF     ; lower limit
+  dw 0          ; lower base
+  db 0          ; lower base 2
+  db 0b10011010 ; reabable code segment with DPL=0
+  db 0b11001111 ; granularity, 32bit segment + upper limit
+  db 0          ; higher base
+GDT_data:
+  dw 0xFFFF     ; lower limit
+  dw 0          ; lower base
+  db 0          ; lower base 2
+  db 0b10010010 ; reabable code segment with DPL=0
+  db 0b11001111 ; granularity, 32bit segment + upper limit
+  db 0          ; higher base
+GDT_end:
+
+GDT_desc:
+  dw GDT_end - GDT_start
+  dd GDT_start
+
 %include "utils.asm"
-test: db "Hello World!", 0
+test: db "Ladon bootloader Version 0", 0x0d, 0x0a, 0 ; 0x0d = CR, 0x0a = LF
+a20_fail: db "Could not enable the A20 line!", 0
+gdt: db "GDT loaded!", 0x0d, 0x0a, 0
+
+[bits 32]
+prot_main:
+  mov ax, 0x08
+  mov cs, ax
+  mov ax, 0x10
+  mov cs, ax
+  mov ds, ax
+  mov es, ax
+  mov fs, ax
+  mov gs, ax
+  mov ss, ax
+  mov esp, 0x7FFFF ; continue to use this location as it should be safe
+
+  mov byte [0xb8000], 'W'
+  hlt
+  jmp prot_main
 
 times 510-($-$$) db 0
 dw 0xAA55
--- utils.asm
+++ utils.asm
@@ -17,6 +17,47 @@ prt_end:
   pop ax
   ret
 
+; print an integer in base 10
+; bx: integer to print
+prti:
+  push ax
+  push cx
+  push dx
+
+  push 0 ; NUL terminator
+  mov dx, 0
+  mov cx, 10
+prti_loop:
+  mov ax, bx
+  div cx
+  mov bx, ax ; move quotient to bx
+
+  mov dh, 0
+  add dl, '0'
+  push dx
+
+  cmp bx, 0
+  je prti_end
+
+  mov dx, 0
+  jmp prti_loop
+
+prti_end:
+  mov ah, 0x0e
+prti_loop2:
+  pop dx
+  cmp dx, 0
+  je prti_end2
+  mov al, dl
+  int 0x10
+
+  jmp prti_loop2
+prti_end2:
+  pop dx
+  pop cx
+  pop ax
+  ret
+
 ; subroutine to check if A20 line is enabled
 ; returns: 1 in ax if it is, otherwise 0
 ck_a20:
