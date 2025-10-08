a11445064f44c4b576d9b2abc89ce3093db640cf
mageOfStructs <jason.puschnig@gmx.at>
2025-10-08 07:43:24 +0200

    initialize ext2 fs

--- stage2.c
+++ stage2.c
@@ -1,6 +1,7 @@
 #include "long.h"
 #include "printf.h"
 #include "ata.h"
+#include "fs/ext2.h"
 #include "serial.h"
 #include <stddef.h>
 #include <stdint.h>
@@ -57,8 +58,9 @@ int main(void) {
     uint32_t *part_lba_start = (uint32_t *)&buf_bytes[0x1BE + 0x8];
     uint32_t *part_lba_secs = (uint32_t *)&buf_bytes[0x1BE + 0xC];
 
-    printf("\nDetected partition at %p with %d sectors (%dM)", *part_lba_start,
-           *part_lba_secs, *part_lba_secs * 512 / 1024 / 1024);
+    println("\nDetected partition at %p with %d sectors (%dM)", *part_lba_start,
+            *part_lba_secs, *part_lba_secs * 512 / 1024 / 1024);
+    init_fs(*part_lba_start);
   }
 
   asm volatile("loop: hlt; jmp loop");
